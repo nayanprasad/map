693396c704864a52b124e03629684e3dbf7e3b7e
Nayan Prasad P K <93549213+nayanprasad@users.noreply.github.com>
2023-09-18 03:47:20 +0530

    added current location

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import './App.css';
+import React, {useEffect, useState} from 'react';
 import {mappls} from 'mappls-web-maps';
 
 
 function App() {
 
+    const [location, setLocation] = useState(null);
+
+    useEffect(() => {
+        // Check if the Geolocation API is available in the browser
+        if ('geolocation' in navigator) {
+            // Use the Geolocation API to get the current location
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+                    setLocation({ latitude, longitude });
+                    console.log(`Latitude: ${latitude}, Longitude: ${longitude}`)
+                },
+                (error) => {
+                    console.error('Error getting location:', error);
+                }
+            );
+        } else {
+            console.error('Geolocation is not available in your browser.');
+        }
+    }, []);
+
     const styleMap = {width: '99%', height: '99vh', display: 'inline-block'}
-    const mapProps = {center: [28.6330, 77.2194], traffic: false, zoom: 10, geolocation: false, clickableIcons: false}
+    const mapProps = {center: [28.6330, 77.2194], traffic: false, zoom: 3, geolocation: false, clickableIcons: false}
     var mapObject;
     var mapplsClassObject = new mappls();
 
@@ -18,26 +40,48 @@ function App() {
             //     position: {lat: 28.5512908, lng: 77.26809282}
             // });
 
-            let start = mapplsClassObject.Marker({
-                map: mapObject,
-                position: {lat: 28.55108, lng: 77.26913},
-                popupHtml: 'Pipe1',
-            });
+            // let start = mapplsClassObject.Marker({
+            //     map: mapObject,
+            //     position: {lat: 28.55108, lng: 77.26913},
+            //     popupHtml: 'Pipe1',
+            // });
+            //
+            // let end = mapplsClassObject.Marker({
+            //     map: mapObject,
+            //     position: {lat: 28.55179, lng: 77.26753},
+            //     popupHtml: 'Pipe2',
+            // });
+            //
+            // let valve = mapplsClassObject.Marker({
+            //     map: mapObject,
+            //     position: {
+            //         lat: 28.55099,
+            //         lng: 77.26849
+            //     },
+            //     popupHtml: 'Valve',
+            // });
 
-            let end = mapplsClassObject.Marker({
-                map: mapObject,
-                position: {lat: 28.55179, lng: 77.26753},
-                popupHtml: 'Pipe2',
-            });
 
-            let valve = mapplsClassObject.Marker({
-                map: mapObject,
-                position: {
-                    lat: 28.55099,
-                    lng: 77.26849
-                },
-                popupHtml: 'Valve',
-            });
+
+            if ('geolocation' in navigator) {
+                // Use the Geolocation API to get the current location
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        const { latitude, longitude } = position.coords;
+                        let currentLocation = mapplsClassObject.Marker({
+                            map: mapObject,
+                            position: {
+                                lat: latitude,
+                                lng: longitude
+                            },
+                            popupHtml: 'Current Location',
+                        });
+                    },
+                    (error) => {
+                        console.error('Error getting location:', error);
+                    }
+                );
+            }
 
 
             var pts =
@@ -94,10 +138,15 @@ function App() {
         })
     });
 
+    const handleAdd = () => {
+
+    }
+
 
     return (
         <div>
             <div id="map" style={styleMap}></div>
+            <div className={"addBtn"} onClick={handleAdd}>ADD</div>
         </div>
     );
 }
